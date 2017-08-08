4f30ed6f88f5d3f116e1d1eb0f42a3b012024465
Muhammad Taha Qadri <Muhammad Taha Qadri>
2017-08-08 14:02:43 +0500

    TodoApp with firebase completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
+import {AngularFireDatabase,FirebaseListObservable} from 'angularfire2/Database';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   items:FirebaseListObservable<any[]>;
+   newItems: string = "" ;
+   itemSend ;
+
+   constructor(public db:AngularFireDatabase){
+    this.items = db.list('/Todos');
+    }
+  AddItems():void {
+    if(this.newItems== "")
+    {
+      alert("Please Enter A Todo");
+    }
+    else{
+      this.items.push(this.newItems);
+      this.newItems= "";
+      // this.itemSend.update({ Todos: this.items });
+    }
+  }
+  removeItem(key):void{
+    const removeItem=this.db.list("/Todos/"+key).remove();
+    removeItem.then(_ => console.log('success'))
+    .catch(err => console.log(err, 'You dont have access!'));
+ }
+ deleteAll():void{
+    const promise = this.db.list('/Todos').remove();
+    promise.then(_ => console.log('success'))
+    .catch(err => console.log(err, 'You dont have access!'));
+
+  }
+ 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/Database';
+
 
 import { AppComponent } from './app.component';
+
+  export const config = {
+    apiKey: "AIzaSyDONsnPXhrd9w7bT2k317IyRc5GtW46xOI",
+    authDomain: "todoappinangular4.firebaseapp.com",
+    databaseURL: "https://todoappinangular4.firebaseio.com",
+    projectId: "todoappinangular4",
+    storageBucket: "todoappinangular4.appspot.com",
+    messagingSenderId: "493900254126"
+  };
+  
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
